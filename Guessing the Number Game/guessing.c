e6549b5820429990a7cc4dddcfb2d4f2b6b5bde1
DaudGit <skhassandaud17@gmail.com>
2021-06-11 02:00:59 +0530

    first commit

---
+++ Guessing the Number Game/guessing.c
@@ -0,0 +1,43 @@
+#include <stdio.H>
+#include <stdlib.h>
+#include <time.h> //for random number generator seed
+
+int main()
+{
+    int RandomNumber = 0;
+    int guess = 0;
+    int NumberOfGuesses;
+    time_t t;
+
+    //Initialization of Random Number generator
+    srand((unsigned)time(&t));
+    //get the random number
+    RandomNumber = rand() % 21;
+    printf("\n This is the guessing game");
+    printf("\n I have choosen a number between 0 and 20 , now you will guess \n");
+    for (NumberOfGuesses = 5; NumberOfGuesses > 0; --NumberOfGuesses)
+    {
+        printf("You have %d tr%s left", NumberOfGuesses, NumberOfGuesses == 1 ? "y" : "ies");
+        printf("\n Enter a guess: ");
+        scanf("%d", &guess);
+
+        if (guess == RandomNumber)
+        {
+            printf("Congratulation !! you guessed it :) \n");
+
+            goto jump;
+        }
+        else if (guess < 0 || guess > 20) //checking for invalid guess
+            printf("I said the number is between 0 and 20 :(");
+
+        else if (RandomNumber > guess)
+            printf("Sorry My number is greater than %d :,( ", guess);
+        else if (RandomNumber < guess)
+            printf("Sorry My number is lesser than %d :,( ", guess);
+    }
+
+    printf(" \n You have had 5 tries and failed.The Number was %d :~(", RandomNumber);
+jump:
+    printf(" Game is Over !!! ");
+    return 0;
+}
---
+++ Tic Tac Toe game/Tic_Tac_Toe.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+int choice, player;
+
+int checkforwin();
+void dispayBoard();
+void markBoard(char mark);
+
+char str1[50];
+char str2[50];
+
+int main()
+{
+    int gamestatus;
+    char mark;
+    printf("Enter player 1 Name \t");
+    gets(str1);
+    printf("Enter player 2 Name \t");
+    gets(str2);
+    player = 1;
+    do
+    {
+        dispayBoard();
+        //change turns
+        player = (player % 2) ? 1 : 2;
+        //get input
+        if (player == 1)
+            printf("%s enter a Number: ", str1);
+        else
+            printf("%s enter a Number: ", str2);
+        scanf("%d", &choice);
+        //set the correct character based on correct player
+        mark = (player == 1) ? 'X' : 'O';
+        //set board based on user choice
+        markBoard(mark);
+        dispayBoard();
+        gamestatus = checkforwin();
+        player++;
+
+    } while (gamestatus == -1);
+    if (gamestatus == 1)
+    {
+        --player;
+        if (player == 1)
+            printf("Congratulation !!! %s is won :)", str1);
+        else
+            printf("Congratulation !!! %s is won :) ", str2);
+    }
+    else
+        printf("The game is draw");
+
+    return 0;
+}
+
+int checkforwin()
+{
+    int returnValue = 0;
+    if (square[1] == square[2] && square[2] == square[3])
+        returnValue = 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+        returnValue = 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+        returnValue = 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+        returnValue = 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+        returnValue = 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+        returnValue = 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+        returnValue = 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+        returnValue = 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3' && square[4] != '4' &&
+             square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
+        returnValue = 0;
+    else
+        returnValue = -1;
+
+    return returnValue;
+}
+/******************************************************
+Function to create Board of Tic Tac Toe with player mark
+*******************************************************/
+void dispayBoard()
+{
+    system("cls");
+    printf("\n \n ******* TIC TAC TOE GAME ******** \n \n");
+    printf("player 1 %s (X) - player 2 %s (O) \n \n ", str1, str2);
+    printf("    |     |     \n");
+    printf("  %c  |  %c  |  %c  \n", square[1], square[2], square[3]);
+    printf("_____|_____|______\n");
+    printf("     |     |      \n");
+    printf("  %c  |  %c  |  %c  \n", square[4], square[5], square[6]);
+    printf("_____|_____|______\n");
+    printf("     |     |      \n");
+    printf("  %c  |  %c  |  %c  \n", square[7], square[8], square[9]);
+    printf("     |     |    \n\n");
+}
+/**********************************
+Set the Board with correct character , X or O in correct spot of array
+***********************************/
+void markBoard(char mark)
+{
+    if (choice == 1 && square[1] == '1')
+        square[1] = mark;
+    else if (choice == 2 && square[2] == '2')
+        square[2] = mark;
+    else if (choice == 3 && square[3] == '3')
+        square[3] = mark;
+    else if (choice == 4 && square[4] == '4')
+        square[4] = mark;
+    else if (choice == 5 && square[5] == '5')
+        square[5] = mark;
+    else if (choice == 6 && square[6] == '6')
+        square[6] = mark;
+    else if (choice == 7 && square[7] == '7')
+        square[7] = mark;
+    else if (choice == 8 && square[8] == '8')
+        square[8] = mark;
+    else if (choice == 9 && square[9] == '9')
+        square[9] = mark;
+    else
+    {
+        --player;
+        printf("invalid move");
+        getch();
+    }
+}
+
+/****************************
+Function to return game status
+1 for game is over with result
+-1 for game is in progress
+0 game is over and no result
+*****************************/
