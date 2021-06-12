b2555a9bd9ce7505839c2bb87748bbbd2cce39f3
DaudGit <skhassandaud17@gmail.com>
2021-06-13 02:14:25 +0530

    new

--- Timer/Timer.c
+++ Timer/Timer.c
@@ -2,38 +2,49 @@
 #include <time.h> //for sleep() function
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 int main()
 {
     int hour, minute, second;
+    int x == 1;
     hour = minute = second = 0;
-    while (1)
+again:
+    printf("Start Timer by pressing 1");
+    scanf("%d", &x);
+    if (x == 1)
     {
-        //clear output screen
-        system("cls");
-        //print time in HH : MM : SS format
-        printf("%02d : %02d : %02d ", hour, minute, second);
-        //clear output buffer in gcc
-        fflush(stdout);
-        //increase second
-        second++;
-        //update hour, minute and second
-        if (second == 60)
+        while (x)
         {
-            minute += 1;
-            second = 0;
+            //clear output screen
+            system("cls");
+            //print time in HH : MM : SS format
+            printf("%02d : %02d : %02d ", hour, minute, second);
+            //clear output buffer in gcc
+            fflush(stdout);
+            //increase second
+            second++;
+            //update hour, minute and second
+            if (second == 60)
+            {
+                minute += 1;
+                second = 0;
+            }
+            if (minute == 60)
+            {
+                hour += 1;
+                minute = 0;
+            }
+            if (hour == 24)
+            {
+                hour = 0;
+                minute = 0;
+                second = 0;
+            }
+            sleep(1); //wait till 1 second
         }
-        if (minute == 60)
-        {
-            hour += 1;
-            minute = 0;
-        }
-        if (hour == 24)
-        {
-            hour = 0;
-            minute = 0;
-            second = 0;
-        }
-        sleep(1); //wait till 1 second
     }
+
+    else
+        goto again;
     return 0;
 }
